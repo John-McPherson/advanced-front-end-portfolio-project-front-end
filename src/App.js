7056663337b45806e84ec5adf42225fb3f8e0db4
John-McPherson <john-mac1989@hotmail.co.uk>
2022-09-03 14:21:29 +0000

    add basic routing to nav bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import styles from './App.module.css';
 import NavBar from './components/NavBar';
+import { Container } from 'react-bootstrap';
+import { Route, Switch } from 'react-router-dom'
+
 
 function App() {
   return (
     <div className={styles.App}>
       <NavBar />
+      <Container className={styles.Main}>
+        <Switch>
+          <Route exact path="/" render={() => <h1>Home page</h1>} />
+          <Route exact path="/addbook" render={() => <h1>Add book</h1>} />
+          <Route exact path="/myprofile" render={() => <h1>Add book</h1>} />
+          <Route exact path="/logout" render={() => <h1>Add book</h1>} />
+        </Switch>
+        {/* <h1>add books</h1>
+        <h1>ny profile</h1>
+        <h1>add logout</h1> */}
+      </Container>
+
 
     </div>
   );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,29 +2,34 @@ import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/images/makecomics-logo.svg";
 import styles from "../assets/css/NavBar.module.css";
+import { NavLink } from 'react-router-dom'
 
 const NavBar = () => {
     return (
         <Navbar expand="md" fixed="top" className={styles.NavBar}>
             <Container className={styles.NavBar__container}>
-                <Navbar.Brand className={styles.NavBar__logo}>
-                    <img src={logo} alt="logo" height="20" />
-                </Navbar.Brand>
+                <NavLink to='/'>
+                    <Navbar.Brand className={styles.NavBar__logo}>
+                        <img src={logo} alt="logo" height="20" />
+                    </Navbar.Brand>
+                </NavLink>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" className={styles.NavBar__toggle} />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ml-auto">
-                        <Nav.Link className={styles.NavBar__link}>
+
+                        <NavLink to='/' className={styles.NavBar__link}>
                             <i className="fas fa-book"></i>my books
-                        </Nav.Link>
-                        <Nav.Link className={styles.NavBar__link}>
+                        </NavLink>
+
+                        <NavLink to='/addbook' className={styles.NavBar__link}>
                             <i className="fas fa-folder-plus"></i>add book
-                        </Nav.Link>
-                        <Nav.Link className={styles.NavBar__link}>
+                        </NavLink>
+                        <NavLink to='/myprofile' className={styles.NavBar__link}>
                             <i className="fas fa-user"></i>my profile
-                        </Nav.Link>
-                        <Nav.Link className={styles.NavBar__link}>
+                        </NavLink>
+                        <NavLink to='/logout' className={styles.NavBar__link}>
                             <i className="fas fa-sign-in-alt"></i>log out
-                        </Nav.Link>
+                        </NavLink>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
