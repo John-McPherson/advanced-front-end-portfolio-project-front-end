dba0c85698e7c9cef9102c64fd40482ff891095b
John-McPherson <john-mac1989@hotmail.co.uk>
2022-09-19 14:33:42 +0000

    add code to pull cover in when updating projects

--- src/project/AllProjects.js
+++ src/project/AllProjects.js
@@ -16,11 +16,13 @@ const AllProjects = () => {
     useEffect(() => {
         const handleMount = async () => {
             try {
-                const [{ data: projects }] = await Promise.all([
+                const [{ data: projects }, { data: pages }] = await Promise.all([
                     axiosReq.get(`/project/`),
+                    axiosReq.get(`/pages/`),
                 ]);
                 setProjects({
                     projects: projects.filter(project => project.is_collaborator),
+                    covers: pages.filter(page => page.is_collaborator && page.title == "Cover"),
                 })
             } catch (err) {
                 console.log(err);
@@ -32,26 +34,29 @@ const AllProjects = () => {
 
     const [projects, setProjects] = useState({
         projects: [],
+        covers: []
     });
 
     return (
         <Container className={styles.main__container}>
+
             <Row>
+
                 {projects.projects.map(project =>
-                    < Col xs={2} className={styles.item} key={project.id}>
+
+                    < Col xs={2} className={styles.item} key={project.id} >
                         <Row>
                             <Col className={styles.item__image_container}>
                                 <Link to={`book/${project.id}`}>
-
-                                    <Image src={defaultImage} className='item__image' />
-                                    {project.is_owner ?
-                                        <Link to={`editbook/${project.id}`}>
-                                            <div className={styles.item__edit_btn}>
-                                                <i className="fa-solid fa-pencil"></i>
-                                            </div>
-                                        </Link>
-                                        : ""}
+                                    <Image src={!projects.covers.filter(cover => cover.project == project.id)[0].letters.includes('default-page_xo6mbk') ? projects.covers.filter(cover => cover.project == project.id)[0].letters : !projects.covers.filter(cover => cover.project == project.id)[0].colors.includes('default-page_xo6mbk') ? projects.covers.filter(cover => cover.project == project.id)[0].colors : !projects.covers.filter(cover => cover.project == project.id)[0].inks.includes('default-page_xo6mbk') ? projects.covers.filter(cover => cover.project == project.id)[0].inks : !projects.covers.filter(cover => cover.project == project.id)[0].roughs.includes('default-page_xo6mbk') ? projects.covers.filter(cover => cover.project == project.id)[0].roughs : defaultImage} className='item__image' />
                                 </Link>
+                                {project.is_owner ?
+                                    <Link to={`editbook/${project.id}`}>
+                                        <div className={styles.item__edit_btn}>
+                                            <i className="fa-solid fa-pencil"></i>
+                                        </div>
+                                    </Link>
+                                    : ""}
                             </Col>
                         </Row>
                         <Row>
@@ -64,6 +69,7 @@ const AllProjects = () => {
                     </Col>
                 )
                 }
+
 
             </Row>
         </Container >
