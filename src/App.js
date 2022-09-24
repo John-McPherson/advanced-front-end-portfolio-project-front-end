9d2742de0aa39b10c0c1bb1be8c0478553d578f1
John-McPherson <john-mac1989@hotmail.co.uk>
2022-09-24 10:22:23 +0000

    remove bootstrap container and style main page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import styles from './App.module.css';
 import NavBar from './components/NavBar';
-import { Container } from 'react-bootstrap';
+
 import { Route, Switch } from 'react-router-dom';
 import './api/axiosDefaults';
 import SignUpForm from './pages/auth/SignUpForm';
@@ -8,7 +8,8 @@ import SignInForm from './pages/auth/SignInForm';
 import { createContext } from 'react';
 import { HomePage } from './components/HomePage';
 import NewProject from './project/NewProject';
-import SingleProject from './project/singleProject';
+import SingleProject from './project/SingleProject';
+import SinglePage from './project/SinglePage';
 
 
 
@@ -26,19 +27,19 @@ function App() {
 
     <div className={styles.App}>
       <NavBar />
-      <Container className={styles.Main}>
+      <div className={styles.Main}>
         <Switch>
           <Route exact path="/" render={() => <HomePage />} />
           <Route exact path="/addbook" render={() => <NewProject />} />
           <Route exact path="/book/:id" render={() => <SingleProject />} />
           <Route exact path="/editbook/:id" render={() => <SingleProject />} />
-          <Route exact path="/page/:id" render={() => <h1>Page placeholder</h1>} />
+          <Route exact path="/page/:id" render={() => <SinglePage />} />
           <Route exact path="/myprofile" render={() => <h1>My Profile</h1>} />
           <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route exact path="/signin" render={() => <SignInForm />} />
           <Route render={() => <h1>404: Page Not Found</h1>} />
         </Switch>
-      </Container>
+      </div>
     </div>
 
   );
