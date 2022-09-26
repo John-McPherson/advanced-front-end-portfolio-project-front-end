c1a986898e3b15c368bf5831d016fc4ea21011e4
John-McPherson <john-mac1989@hotmail.co.uk>
2022-09-26 17:38:07 +0000

    add filter to reduce load time

--- src/project/AllProjects.js
+++ src/project/AllProjects.js
@@ -10,6 +10,7 @@ import styles from "../assets/css/Summary.module.css"
 import { Link } from "react-router-dom";
 
 
+
 const AllProjects = () => {
 
 
@@ -18,12 +19,15 @@ const AllProjects = () => {
             try {
                 const [{ data: projects }, { data: pages }] = await Promise.all([
                     axiosReq.get(`/project/`),
-                    axiosReq.get(`/pages/`),
+                    axiosReq.get(`/pages/?project=&title=cover`),
                 ]);
+                console.log(projects.filter(project => project.is_collaborator || project.is_owner))
+
                 setProjects({
-                    projects: projects.filter(project => project.is_collaborator),
-                    covers: pages.filter(page => page.is_collaborator && page.title == "Cover"),
+                    projects: projects.filter(project => project.is_collaborator || project.is_owner),
+                    covers: pages
                 })
+                // console.log(projects)
             } catch (err) {
                 console.log(err);
             }
@@ -37,26 +41,41 @@ const AllProjects = () => {
         covers: []
     });
 
-    return (
-        <Container className={styles.main__container}>
 
-            <Row>
+
+    return (
+        <Container className={styles.main__container} >
+
+            <Row className="justify-content-center">
+                {projects.projects.length ? "" : <Col xs={12} className={styles.emptyProjectContainer}>
+                    <div>
+
+                        <i className="fa-solid fa-face-sad-cry"></i>
+                        <h2>{projects.project}</h2>
+                        <Link to={'/addbook'}>Click here to get started</Link>
+                    </div>
+                </Col>
+                }
+
 
                 {projects.projects.map(project =>
 
-                    < Col xs={2} className={styles.item} key={project.id} >
+                    < Col xs={4} md={2} className={styles.item} key={project.id} >
                         <Row>
-                            <Col className={styles.item__image_container}>
+                            <Col xs={12} className={styles.item__image_container}>
+
                                 <Link to={`book/${project.id}`}>
+                                    {/* <p>sdglfk</p> */}
+                                    {console.log(projects.covers)}
                                     <Image src={!projects.covers.filter(cover => cover.project == project.id)[0].letters.includes('default-page_xo6mbk') ? projects.covers.filter(cover => cover.project == project.id)[0].letters : !projects.covers.filter(cover => cover.project == project.id)[0].colors.includes('default-page_xo6mbk') ? projects.covers.filter(cover => cover.project == project.id)[0].colors : !projects.covers.filter(cover => cover.project == project.id)[0].inks.includes('default-page_xo6mbk') ? projects.covers.filter(cover => cover.project == project.id)[0].inks : !projects.covers.filter(cover => cover.project == project.id)[0].roughs.includes('default-page_xo6mbk') ? projects.covers.filter(cover => cover.project == project.id)[0].roughs : defaultImage} className='item__image' />
                                 </Link>
-                                {project.is_owner ?
+                                {/* {project.is_owner ?
                                     <Link to={`editbook/${project.id}`}>
                                         <div className={styles.item__edit_btn}>
                                             <i className="fa-solid fa-pencil"></i>
                                         </div>
                                     </Link>
-                                    : ""}
+                                    : ""} */}
                             </Col>
                         </Row>
                         <Row>
