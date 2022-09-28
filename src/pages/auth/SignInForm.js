a7f621ff33d56fd46a98feb07abb6588fc8030b5
John-McPherson <john-mac1989@hotmail.co.uk>
2022-09-28 10:56:38 +0000

    fix typo

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -43,45 +43,45 @@ const SignUpForm = () => {
   }
 
   return (
-        <Form onSubmit={handleSubmit}>
-            <Row className={`${styles.Form__Container} ${styles.Form__Container__Signin}`} >
+    <Form onSubmit={handleSubmit}>
+      <Row className={`${styles.Form__Container} ${styles.Form__Container__Signin}`} >
 
-                <Col className={` ${styles.Form__Container__Col}`} >
-                    <Container className={appStyles.Remove__margins_paddings} >
+        <Col className={` ${styles.Form__Container__Col}`} >
+          <Container className={appStyles.Remove__margins_paddings} >
 
-                        <Form.Group controlId="username" className={styles.Form__Input_Group} >
-                            <span><i className="fa-solid fa-user"></i></span>
-                            <Form.Label className="d-none">Username</Form.Label>
-                            <Form.Control type="text" placeholder="username" name='username' value={username}
-                                onChange={handleChange} />
-                        </Form.Group>
-                        {errors.username?.map((message, idx) => (
-                            <p key={idx} className={styles.Form__Input_Warning}>
-                                {message}
-                            </p>
-                        ))}
+            <Form.Group controlId="username" className={styles.Form__Input_Group} >
+              <span><i className="fa-solid fa-user"></i></span>
+              <Form.Label className="d-none">Username</Form.Label>
+              <Form.Control type="text" placeholder="username" name='username' value={username}
+                onChange={handleChange} />
+            </Form.Group>
+            {errors.username?.map((message, idx) => (
+              <p key={idx} className={styles.Form__Input_Warning}>
+                {message}
+              </p>
+            ))}
 
-                        <Form.Group controlId="password" className={styles.Form__Input_Group}>
-                            <span><i className="fa-solid fa-lock"></i></span>
-                            <Form.Label className="d-none">Password</Form.Label>
-                            <Form.Control type="password" name='password' placeholder="password" value={password} onChange={handleChange} />
-                        </Form.Group>
-                        {errors.password?.map((message, idx) => (
-                            <p key={idx} className={styles.Form__Input_Warning}>
-                                {message}
-                            </p>
-                        ))}
+            <Form.Group controlId="password" className={styles.Form__Input_Group}>
+              <span><i className="fa-solid fa-lock"></i></span>
+              <Form.Label className="d-none">Password</Form.Label>
+              <Form.Control type="password" name='password' placeholder="password" value={password} onChange={handleChange} />
+            </Form.Group>
+            {errors.password?.map((message, idx) => (
+              <p key={idx} className={styles.Form__Input_Warning}>
+                {message}
+              </p>
+            ))}
 
-                        <Button type="submit" className={appStyles.Btn}>
-                            sign up
-                        </Button>
+            <Button type="submit" className={appStyles.Btn}>
+              sign in
+            </Button>
 
-                    </Container>
+          </Container>
 
-                </Col>
+        </Col>
 
-            </Row >
-        </Form >
+      </Row >
+    </Form >
   )
 }
 
