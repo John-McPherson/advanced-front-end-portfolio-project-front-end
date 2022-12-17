7d2f925b5e354b5844b82891962a6a2ce437d923
John-McPherson <john-mac1989@hotmail.co.uk>
2022-12-17 16:45:26 +0000

    add loading to allprojects

--- src/project/AllProjects.js
+++ src/project/AllProjects.js
@@ -4,6 +4,7 @@ import { Col, Container, Image, Row } from 'react-bootstrap'
 import defaultImage from '../assets/images/default-page.png'
 import styles from '../assets/css/Summary.module.css'
 import { Link } from 'react-router-dom'
+import Loading from '../components/Loading'
 
 const AllProjects = () => {
   useEffect(() => {
@@ -16,7 +17,8 @@ const AllProjects = () => {
 
         setProjects({
           projects: projects.filter(project => project.is_collaborator || project.is_owner),
-          covers: pages
+          covers: pages,
+          loaded: true
         })
       } catch (err) {
 
@@ -27,51 +29,58 @@ const AllProjects = () => {
 
   const [projects, setProjects] = useState({
     projects: [],
-    covers: []
+    covers: [],
+    loaded: false
   })
 
   return (
-    <Container className={styles.main__container} >
+    <React.Fragment>
+      {projects.loaded
 
-      <Row className="justify-content-center">
-        {projects.projects.length
-          ? ''
-          : <Col xs={12} className={styles.emptyProjectContainer}>
-            <div>
+        ? <Container className={styles.main__container} >
 
-              <i className="fa-solid fa-face-sad-cry"></i>
-              <h2>{projects.project}</h2>
-              <Link to={'/addbook'}>Click here to get started</Link>
-            </div>
-          </Col>
-        }
+          <Row className="justify-content-center">
+            {projects.projects.length
+              ? ''
+              : <Col xs={12} className={styles.emptyProjectContainer}>
+                <div>
 
-        {projects.projects.map(project =>
-
-          < Col xs={4} md={2} className={styles.item} key={project.id} >
-            <Row>
-              <Col xs={12} className={styles.item__image_container}>
-
-                <Link to={`book/${project.id}`}>
-
-                  <Image src={!projects.covers.filter(cover => cover.project === project.id)[0].letters.includes('default-page_xo6mbk') ? projects.covers.filter(cover => cover.project === project.id)[0].letters : !projects.covers.filter(cover => cover.project === project.id)[0].colors.includes('default-page_xo6mbk') ? projects.covers.filter(cover => cover.project === project.id)[0].colors : !projects.covers.filter(cover => cover.project === project.id)[0].inks.includes('default-page_xo6mbk') ? projects.covers.filter(cover => cover.project === project.id)[0].inks : !projects.covers.filter(cover => cover.project === project.id)[0].roughs.includes('default-page_xo6mbk') ? projects.covers.filter(cover => cover.project === project.id)[0].roughs : defaultImage} className='item__image' />
-                </Link>
-
+                  <i className="fa-solid fa-face-sad-cry"></i>
+                  <h2>{projects.project}</h2>
+                  <Link to={'/addbook'}>Click here to get started</Link>
+                </div>
               </Col>
-            </Row>
-            <Row>
-              <Col>
-                <Link to={`book/${project.id}`}>
-                  <h2>{project.title}</h2>
-                </Link>
-              </Col>
-            </Row>
-          </Col>
-        )
-        }
+            }
 
-      </Row>
-    </Container >
+            {projects.projects.map(project =>
+
+              < Col xs={4} md={2} className={styles.item} key={project.id} >
+                <Row>
+                  <Col xs={12} className={styles.item__image_container}>
+
+                    <Link to={`book/${project.id}`}>
+
+                      <Image src={!projects.covers.filter(cover => cover.project === project.id)[0].letters.includes('default-page_xo6mbk') ? projects.covers.filter(cover => cover.project === project.id)[0].letters : !projects.covers.filter(cover => cover.project === project.id)[0].colors.includes('default-page_xo6mbk') ? projects.covers.filter(cover => cover.project === project.id)[0].colors : !projects.covers.filter(cover => cover.project === project.id)[0].inks.includes('default-page_xo6mbk') ? projects.covers.filter(cover => cover.project === project.id)[0].inks : !projects.covers.filter(cover => cover.project === project.id)[0].roughs.includes('default-page_xo6mbk') ? projects.covers.filter(cover => cover.project === project.id)[0].roughs : defaultImage} className='item__image' />
+                    </Link>
+
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>
+                    <Link to={`book/${project.id}`}>
+                      <h2>{project.title}</h2>
+                    </Link>
+                  </Col>
+                </Row>
+              </Col>
+            )
+            }
+
+          </Row>
+        </Container >
+        : <Loading />
+      }
+    </React.Fragment >
   )
 }
 
